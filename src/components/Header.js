2bd3a38f971f922d11de8739e1709801f11a373e
anjitzero <ankitpariyar486@gmail.com>
2021-02-21 16:25:05 +0545

    committed from anjitBranch

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Check Header edited by anjit</div>;
+  return (
+    <header>
+      <i class="fa fa-long-arrow-down" aria-hidden="true"></i>
+      <Menu />
+      <div>Check Header edited by anjit -- Sonang Rai</div>
+    </header>
+  );
 };
 
 export default Header;
